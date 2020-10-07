18cd1ce514bfd6197be0c1fbf0c72d511f83b178
junhee <wjunhee102@gmail.com>
2020-10-08 08:26:18 +0900

    temp: redux todos

--- src/store/modules/index.ts
+++ src/store/modules/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import counter from './counter';
+import todos   from './todos';
 
 const rootReducer = combineReducers({
-  counter
+  counter,
+  todos
 });
 
 export default rootReducer;
