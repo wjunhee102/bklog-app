4c2e4dc74cd442730436f7fff6dd84e55628f736
jh <jh.hwang@softwareinlife.com>
2020-10-19 18:02:07 +0900

    fearture: styled-components 추가

---
+++ src/components/WorkItem.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  color: #ff0000;
+  
+`;
+
+function WorkItem() {
+  return (
+    <div className="work-item">
+      <Container>
+
+      </Container>
+    </div>
+  )
+}
+
+export default WorkItem;
