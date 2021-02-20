b9a586501c8b850657c7a0f5130a6747acb0a5dd
junhee <wjunhee102@gmail.com>
2021-02-21 01:38:43 +0900

    feat: hashRouter 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, HashRouter} from 'react-router-dom';
 import Home from './pages/Home';
 import Editor from './components/editor/index';
 import BkPage from './pages/BkPage';
+import AuthPage from './pages/AuthPage';
 
 import './App.scss';
 
+// hashRouter?
 function App() {
   return (
     <div className="App black-mode">
-      <Router>
+      <HashRouter basename="/bk">
         <Switch>
           <Route path='/' component={Home} exact />
           <Route path='/edit' component={Editor} />
           <Route path='/bklog/:id' component={BkPage} />
+          <Route path='/auth/:id' component={AuthPage} />
         </Switch>
-      </Router>
+      </HashRouter>
     </div>
   );
 }
