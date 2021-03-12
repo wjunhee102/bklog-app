03dcfd5a5787e6bb01f9336806c277103898827a
junhee <wjunhee102@gmail.com>
2021-03-12 23:43:09 +0900

    Updates

---
+++ precache-manifest.9bad0523022a4caf7ac3ad69c951ffef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da38ee832f7d571d0d232b737e136d68",
+    "url": "/bklog/index.html"
+  },
+  {
+    "revision": "2e6001951da6faf87db9",
+    "url": "/bklog/static/css/main.58bf2a6d.chunk.css"
+  },
+  {
+    "revision": "5a9a085e18a1c7a401aa",
+    "url": "/bklog/static/js/2.bd3f6491.chunk.js"
+  },
+  {
+    "revision": "1f3ec544f59db43d224e4cd99b0c3b1d",
+    "url": "/bklog/static/js/2.bd3f6491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e6001951da6faf87db9",
+    "url": "/bklog/static/js/main.54659d8c.chunk.js"
+  },
+  {
+    "revision": "baaae3fc530f3b6657b9",
+    "url": "/bklog/static/js/runtime-main.80799a01.js"
+  }
+]);
