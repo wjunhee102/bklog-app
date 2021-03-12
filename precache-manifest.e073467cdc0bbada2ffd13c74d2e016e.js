b5bb20184eccf3af83045d683089807702895860
junhee <wjunhee102@gmail.com>
2021-03-13 00:08:22 +0900

    Updates

--- precache-manifest.e073467cdc0bbada2ffd13c74d2e016e.js
+++ precache-manifest.e073467cdc0bbada2ffd13c74d2e016e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8a63fe19042823bea92a3a169a8c4914",
+    "revision": "0ecb7c7756d5d8c46e5637c02f5d6e89",
     "url": "/bklog-app/index.html"
   },
   {
-    "revision": "68061b7f740a34ba7990",
+    "revision": "2e6001951da6faf87db9",
     "url": "/bklog-app/static/css/main.58bf2a6d.chunk.css"
   },
   {
-    "revision": "75faf9972a4fbf6d1868",
-    "url": "/bklog-app/static/js/2.14c2bae2.chunk.js"
+    "revision": "5a9a085e18a1c7a401aa",
+    "url": "/bklog-app/static/js/2.bd3f6491.chunk.js"
   },
   {
     "revision": "1f3ec544f59db43d224e4cd99b0c3b1d",
-    "url": "/bklog-app/static/js/2.14c2bae2.chunk.js.LICENSE.txt"
+    "url": "/bklog-app/static/js/2.bd3f6491.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "68061b7f740a34ba7990",
-    "url": "/bklog-app/static/js/main.e7b6f1b8.chunk.js"
+    "revision": "2e6001951da6faf87db9",
+    "url": "/bklog-app/static/js/main.54659d8c.chunk.js"
   },
   {
     "revision": "c39472bc889adc96b410",
