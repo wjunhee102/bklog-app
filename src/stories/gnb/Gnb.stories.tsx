9c8edcc4b623300f9e2f25e7d60deaef9420179a
junhee <wjunhee102@gmail.com>
2021-11-27 19:40:55 +0900

    #12 [feat] gnb story 생성

---
+++ src/stories/gnb/Gnb.stories.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import '../../assets/index.css';
+import GnbComponent from '../../components/gnb/GnbComponent';
+import FullSizeContainer from '../common/FullScreenContainer';
+import { UserInfo } from '../../store/modules/auth/utils';
+import { UseGnbConnectStoreType } from '../../components/gnb/hooks/useGnbConnectStore';
+
+export default {
+  title: 'Gnb/gnb',
+  component: GnbComponent
+} as ComponentMeta<typeof GnbComponent>
+
+const dummyUseConnectStore = ( 
+  toggle: boolean, 
+  loading: boolean, 
+  user: UserInfo 
+) => ({
+  pageToggle: toggle,
+  onChangeToggle: (toogle1?: boolean) => console.log(toogle1),
+  loading,
+  user,
+  onSignOutUser: () => {}
+});
+
+const userInfo = {
+  penName: "test",
+  profileId: "test123!22",
+  photo: null,
+  coverImage: null,
+  coverColor: null,
+  bio: null,
+  firstName: "test",
+  lastName: "test",
+  email: "test@test.com"
+}
+
+const Template: ComponentStory<typeof GnbComponent> = args => (
+  <FullSizeContainer>
+    <GnbComponent {...args} />
+  </FullSizeContainer>
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  useConnectStore: dummyUseConnectStore(false, false, userInfo)
+}
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  useConnectStore: dummyUseConnectStore(true, true, userInfo)
+}
