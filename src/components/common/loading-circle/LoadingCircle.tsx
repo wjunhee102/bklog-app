d578efc12acbc9fa5897fd8eebe1dbe4888bff35
junhee <wjunhee102@gmail.com>
2021-12-03 08:46:08 +0900

    #11 [fix] error 수정

--- src/components/common/loading-circle/LoadingCircle.tsx
+++ src/components/common/loading-circle/LoadingCircle.tsx
@@ -5,7 +5,7 @@ const LoadingCircle: React.FC = () => {
   return (
     <div>
       <svg className="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
-        <circle className="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+        <circle className="path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30"></circle>
       </svg>
     </div>
   );
