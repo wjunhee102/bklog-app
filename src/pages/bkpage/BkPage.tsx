fa0937669d9734b118e487fff31443ef2ce407db
junhee <wjunhee102@gmail.com>
2021-12-03 09:05:11 +0900

    #2 [refactor] hooks 분리

--- src/pages/bkpage/BkPage.tsx
+++ src/pages/bkpage/BkPage.tsx
@@ -1,54 +1,19 @@
 import React from 'react';
 import classNames from 'classnames';
-import useAuth from '../../hooks/useAuth';
-import useBklog from '../../hooks/useBKlog';
-import { Route, useHistory, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import ErrorPopup from '../../components/base/popup/ErrorPopup';
-import { History } from 'history';
 import NotFoundPage from '../NotFoundPage';
 import PenNameRoute from './route/PenNameRoute';
 import IdRoute from './route/IdRoute';
-
-function handleErrorPopup({
-  type, 
-  code
-}: { type: string, code: string | number}, 
-  history?: History<unknown>){
-    return () => {
-      if(type === "AUTH" && code == "002" && history) {
-        history.push("/home");
-      }
-  } 
-}
+import useBkPage from './hooks/useBkPage';
 
 const BkPage: React.FC = () => {
 
   const {
-    onCheckToken
-  } = useAuth();
-
-  const {
-    bklogState,
-    onGetPage
-  } = useBklog();
-
-  const history = useHistory();
-
-  const handleCallback = bklogState.error? handleErrorPopup({ type: bklogState.error.type, code: bklogState.error.code }, history) : null;
-
-  // useEffect(() => {
-  //   // onCheckToken();
-  // });
-
-  // useEffect(() => {
-  //   if(match.params.id) onGetPage(match.params.id);
-  // }, [match.params.id]);
-
-  // useEffect(() => {
-  //   console.log(match, match.params.penName);
-  // }, [match]);
-
-  // if(!match.params.penName) return (<Redirect to="/home" />)
+    errMessage,
+    errorToggle,
+    handleCallback 
+  } = useBkPage();
 
   return (
     <div className={classNames("bk-page", "h-full", "overflow-scroll")}>
@@ -58,7 +23,7 @@ const BkPage: React.FC = () => {
         <Route component={NotFoundPage} />
       </Switch>
       
-      { bklogState.error? <ErrorPopup error={bklogState.error} callback={handleCallback} /> : null }
+      <ErrorPopup message={errMessage} callback={handleCallback} toggle={errorToggle} />
       
     </div>
   );
---
+++ src/pages/bkpage/hooks/useBkPage.ts
@@ -0,0 +1,66 @@
+import { useHistory } from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
+import useBKlog from "../../../hooks/useBKlog";
+import { History } from 'history';
+import { useEffect, useState } from "react";
+import useStoreReset from "../../../hooks/useStoreReset";
+
+function handleErrorPopup({
+  type, 
+  code
+}: { type: string, code: string | number}, 
+  history?: History<unknown>){
+    return () => {
+      if(type === "AUTH" && code == "002" && history) {
+        history.push("/home");
+      }
+  } 
+}
+
+function useBkPage() {
+  const [ errMessage, setErrMessage ] = useState<string>(null);
+  const [ errorToggle, setErrToggle ] = useState<boolean>(false);
+
+  const {
+    authState,
+    onCheckToken,
+  } = useAuth();
+
+  const {
+    bklogState
+  } = useBKlog();
+
+  const {
+    onAllReset
+  } = useStoreReset();
+
+  const history = useHistory();
+
+  const handleCallback = () => {
+    onAllReset();
+    history.push("/home");
+  } 
+
+  useEffect(() => {
+    if(bklogState.error) {
+      if(bklogState.error.type === "AUTH" && bklogState.error.code === "002") {
+        setErrToggle(true);
+        setErrMessage(bklogState.error.message);
+      }
+    } else if(authState.error) {
+      setErrToggle(authState.errorToggle);
+      setErrMessage(authState.error.message);
+    } else {
+      setErrToggle(false);
+      setErrMessage(null);
+    }
+  }, [bklogState.error, authState.error]);
+
+  return {
+    handleCallback,
+    errorToggle,
+    errMessage
+  }
+}
+
+export default useBkPage;
