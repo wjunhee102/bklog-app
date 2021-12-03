1161070c6017a34a6b231aa78304ec30e873a299
junhee <wjunhee102@gmail.com>
2021-12-03 09:06:32 +0900

    #11 [style] home -> homepage 변경

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect, useHistory} from 'react-router-dom';
 import AuthPage from './pages/AuthPage';
 import useBase from './hooks/useBase';
-import Home from './pages/HomePage';
+import HomePage from './pages/homepage';
 import BkPage from './pages/bkpage';
 import Gnb from './components/gnb';
 import classNames from 'classnames';
@@ -19,29 +19,31 @@ const App: React.FC = () => {
     onReSignInUser
   } = useAuth();
 
-  useEffect(() => {
-    if(waitingCount && user) {
-      if(waitingCount <= 5) {
-        const timer = setTimeout(() => {
-          onReissueToken();
-        }, 2000);
-        console.log(waitingCount);
+  // useEffect(() => {
+  //   if(waitingCount && user) {
+  //     if(waitingCount <= 5) {
+  //       const timer = setTimeout(() => {
+  //         onReissueToken();
+  //       }, 2000);
+  //       console.log(waitingCount);
 
-        return () => clearTimeout(timer);
-      } else {
-        //onAllReset();
-        /**
-         * 500 page로 redirect
-         */
-      }
-    }
-  }, [waitingCount]);
+  //       return () => clearTimeout(timer);
+  //     } else {
+  //       //onAllReset();
+  //       /**
+  //        * 500 page로 redirect
+  //        */
+  //     }
+  //   }
+  // }, [waitingCount]);
 
   useEffect(() => {
     if(!user) {
       onReSignInUser();
     }
   }, []);
+
+  // all reset시 Home으로 이동
 
   return (
     <div className={classNames(
@@ -56,7 +58,7 @@ const App: React.FC = () => {
           <Route path="/" exact>
             <Redirect to="/home" />
           </Route>
-          <Route exact path="/home" component={Home} />
+          <Route exact path="/home" component={HomePage} />
           <Route path="/bklog" component={BkPage} />
           <Route path="/auth" component={AuthPage} />
           <Route component={NotFoundPage} />
