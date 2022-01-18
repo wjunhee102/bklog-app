80032e09fd268fe0783b31ca2e462bb4072edd0a
junhee <wjunhee102@gmail.com>
2022-01-19 00:20:54 +0900

    #11 [docs] error code 규칙 주석 추가

--- src/utils/api-utils/api.ts
+++ src/utils/api-utils/api.ts
@@ -2,6 +2,21 @@ import axios, { AxiosRequestConfig, AxiosInstance, AxiosResponse, AxiosError } f
 import { stringify } from 'qs';
 import { ApiErrorType, IRestObject, IGqlObject } from '.';
 
+/**
+  
+  error code 규칙
+  
+  - 000: 정해지지 않은 error
+  - 001: 재시도
+  - 002: 인증 불일치
+  - 003: 권한이 없음
+  - 004: 정보를 찾을 수 없음
+  - 005: 새로 고침
+  - 006: 단순 error
+  - 007: System error
+  
+*/
+
 export class ApiError implements ApiErrorType {
   type: string = "unknown";
   code: string | number = "000";
