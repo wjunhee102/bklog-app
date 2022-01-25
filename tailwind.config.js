659a25c719d82157f23cf5ba38720b5955f8fdd0
junhee <wjunhee102@gmail.com>
2022-01-25 14:44:39 +0900

    #11 [refactor] 설정 색 변경  - cool gray -> gray

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
 
       black: colors.black,
       white: colors.white,
-      gray: colors.coolGray,
+      gray: colors.gray,
       red: colors.red,
       yellow: colors.amber,
       green: colors.emerald,
